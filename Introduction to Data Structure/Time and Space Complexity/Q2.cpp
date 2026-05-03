4de829a9f9acf713d2055f3ce14b5c3d49e4f479
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-05-04 00:33:38 +0600

    Add files via upload

---
+++ Introduction to Data Structure/Time and Space Complexity/Q2.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i+=10)
+    {
+        for(int j=n;j>=0;j--)
+        {
+            cout << "Hello" << endl;
+        }
+    }
+
+
+    for(int i=0;i<n;i++)
+    {
+        cout << "Hi" << endl;
+    }
+
+    return 0;
+}
+
+// Time Complexity -> O(n*n)
