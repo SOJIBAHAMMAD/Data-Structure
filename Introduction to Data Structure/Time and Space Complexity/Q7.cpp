5ee25176382c805c7db558baad7105ce26a8dfd7
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-05-04 00:34:23 +0600

    Add files via upload

---
+++ Introduction to Data Structure/Time and Space Complexity/Q7.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        sort(a,a+n);
+    }
+
+    return 0;
+}
+
+// Time Complexity -> O(n*nlog(n)
