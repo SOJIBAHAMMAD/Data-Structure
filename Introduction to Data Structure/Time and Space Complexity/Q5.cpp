733630d576956393011ef269e02b580d700d8e89
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-05-04 00:34:03 +0600

    Add files via upload

---
+++ Introduction to Data Structure/Time and Space Complexity/Q5.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+    int n;
+    cin>>n;
+    int sum=0;
+    for(int i=0;i*i<n;i++)
+    {
+        sum += i;
+    }
+
+    int k;
+    cin>>k;
+    for(int i=0;i<n;i++)
+    {
+        sum += i;
+        i*=k;
+    }
+
+
+
+    return 0;
+}
+
+// Time Complexity -> O(sqrt(n))+ O(log(k base)(n))
+
+// So finally TC is O(sqrt(n))
