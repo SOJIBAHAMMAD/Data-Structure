36c6737470670831b752eab422e7eb9c350a5bc3
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-05-08 03:29:47 +0600

    Create Q1-Duplicate.cpp

---
+++ Introduction to Data Structure/Assignment-01/Q1-Duplicate.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+    int N;
+    cin>>N;
+    vector<int>A(N);
+    for(int i=0;i<N;i++) {
+        cin>>A[i];
+    }
+    sort(A.begin(),A.end());
+    bool flag=false;
+    for(int i=0;i<N-1;i++) {
+        if(A[i]==A[i+1]) {
+            flag=true;
+            break;
+        }
+    }
+    if(flag) {
+        cout<<"YES"<<endl;
+    } else {
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
