48ce6b18ff55229195202ff1173e35411256b270
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-05-11 00:08:39 +0600

    Add files via upload

---
+++ Singly Linked List/Insertion Operation/Q4-Insert_At_Tail_Optimize.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int val;
+    Node* next;
+
+    Node(int val) {
+        this->val=val;
+        this->next=NULL;
+    }
+};
+void InsertAtTail(Node* &head,Node* &tail,int val) {
+    Node* newNode = new Node(val);
+    if(head==NULL) {
+        head=newNode;
+        tail=newNode;
+        return;
+    }
+    tail->next=newNode;
+    tail=tail->next;
+    // or tail=newNode;
+}
+
+void printingLinkedList(Node* &head) {
+    Node* temp=head;
+    while(temp!=NULL) {
+        cout<<temp->val<<"->";
+        temp=temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+int main () {
+    Node* head = NULL;
+    Node* tail = NULL;
+    while(1) {
+        int val;
+        cin>>val;
+        if(val==-1) {
+            break;
+        }
+        InsertAtTail(head,tail,val);
+    }
+    printingLinkedList(head);
+    return 0;
+}
