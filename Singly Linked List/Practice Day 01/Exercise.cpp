7f50fee4a918ca68e34815bc972160eb24c1660a
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-05-12 00:02:15 +0600

    Create Exercise.cpp

---
+++ Singly Linked List/Practice Day 01/Exercise.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int val;
+    Node* next;
+
+    Node(int val) {
+        this->val=val;
+        this->next=NULL;
+    }
+};
+void InsertAtTail(Node* &head,Node* &tail,int val) {
+    Node* newNode = new Node(val);
+    if(head==NULL) {
+        head=newNode;
+        tail=newNode;
+        return;
+    }
+    tail->next=newNode;
+    tail=newNode;
+}
+void duplicate(Node* &head) {
+    bool flag=false;
+    Node* temp=head;
+    for(Node* temp=head;temp->next->next!=NULL;temp=temp->next) {
+        if(temp->val == temp->next->val) {
+            flag=true;
+            break;
+        }
+    }
+    if(flag) {
+        cout<<"YES"<<endl;
+    } else {
+        cout<<"NO"<<endl;
+    }
+}
+int main () {
+    Node* head=NULL;
+    Node* tail=NULL;
+    while(1) {
+        int value;
+        cin>>value;
+        if(value==-1) {
+            break;
+        }
+        InsertAtTail(head,tail,value);
+    }
+    duplicate(head);
+    return 0;
+}
