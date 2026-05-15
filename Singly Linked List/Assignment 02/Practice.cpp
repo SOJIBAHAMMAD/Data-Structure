5bc43490962aadae3ece5e6113389b58701045e4
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-05-16 00:16:37 +0600

    Add files via upload

---
+++ Singly Linked List/Assignment 02/Practice.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* next;
+
+    Node(int val) {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void delete_duplicate(Node* &head) {
+    Node* temp1 = head;
+
+    while (temp1 != NULL) {
+        Node* temp2 = temp1;
+
+        while (temp2->next != NULL) {
+            if (temp2->next->val == temp1->val) {
+                // Duplicate found, delete temp2->next
+                Node* nodeToDelete = temp2->next;
+                temp2->next = temp2->next->next;
+                delete nodeToDelete;
+            } else {
+                temp2 = temp2->next;
+            }
+        }
+
+        temp1 = temp1->next;
+    }
+}
+
+void InsertAtTail(Node* &head, Node* &tail, int val) {
+    Node* newNode = new Node(val);
+    if (head == NULL) {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void print_linked_list(Node* head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    while (true) {
+        int x;
+        cin >> x;
+        if (x == -1) break;
+        InsertAtTail(head, tail, x);
+    }
+
+    delete_duplicate(head);
+    print_linked_list(head);
+
+    return 0;
+}
