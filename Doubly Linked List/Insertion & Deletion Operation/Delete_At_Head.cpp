353f0bcf5dd579f3327c8cdc6fe03c308c170227
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-05-18 00:05:31 +0600

    Add files via upload

---
+++ Doubly Linked List/Insertion & Deletion Operation/Delete_At_Head.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int val;
+    Node* next;
+    Node* prev;
+
+    Node(int val) {
+        this->val=val;
+        this->next=NULL;
+        this->prev=NULL;
+    }
+};
+void DeleteAtHead(Node* &head,Node* &tail) {
+
+    if(head==NULL) {
+        return;
+    }
+
+    Node* nodeToDelete=head;
+    head=head->next;
+    delete(nodeToDelete);
+    if(head==NULL) {
+        tail=NULL;
+        return;
+    }
+    head->prev=NULL;
+}
+void InsertAtTail(Node* &head,Node* &tail,int val) {
+    Node* newNode=new Node(val);
+    if(head==NULL) {
+        head=newNode;
+        tail=newNode;
+        return;
+    }
+    tail->next=newNode;
+    newNode->prev=tail;
+    tail=newNode;
+}
+void printing_DLL(Node* &head) {
+    Node* temp=head;
+    while(temp!=NULL) {
+        cout<<temp->val<<"<->";
+        temp=temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+int main () {
+    Node* head=NULL;
+    Node* tail=NULL;
+
+    while(1) {
+        int val;
+        cin>>val;
+        if(val==-1) {
+            break;
+        }
+        InsertAtTail(head,tail,val);
+    }
+    printing_DLL(head);
+    DeleteAtHead(head,tail);
+    printing_DLL(head);
+    return 0;
+}
