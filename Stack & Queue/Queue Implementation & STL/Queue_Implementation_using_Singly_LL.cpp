1ff87fbaa0aee4f69d30a4ef38fc02a1b5408731
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-05-29 17:51:58 +0600

    Add files via upload

---
+++ Stack & Queue/Queue Implementation & STL/Queue_Implementation_using_Singly_LL.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int val;
+    Node* next;
+
+    Node(int val) {
+        this->val=val;
+        this->next=NULL;
+    }
+};
+class myQueue{
+    public:
+    Node* head=NULL;
+    Node* tail=NULL;
+    int sz=0;
+
+    void push(int val) {
+        Node* newNode=new Node(val);
+        sz++;
+        if(head==NULL) {
+            head=newNode;
+            tail=newNode;
+            return;
+        }
+        tail->next=newNode;
+        tail=newNode;
+    }
+
+    void pop() {
+        sz--;
+        Node* nodeTodelete=head;
+        head=head->next;
+        delete nodeTodelete;
+        if(head==NULL) {
+            tail=NULL;
+        }
+    }
+
+    int front() {
+        return head->val;
+    }
+
+    int back() {
+        return tail->val;
+    }
+
+    int size() {
+        return sz;
+    }
+
+    bool empty() {
+        return head==NULL;
+    }
+
+};
+int main () {
+    myQueue qu;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++) {
+        int val;
+        cin>>val;
+        qu.push(val);
+    }
+    //cout<<qu.front()<<" "<<qu.back()<<" "<<qu.size()<<endl;
+    while(!qu.empty()) {
+        cout<<qu.front()<<" ";
+        qu.pop();
+    }
+    return 0;
+}
