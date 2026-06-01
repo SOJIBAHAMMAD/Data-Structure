338d3918d03beb5c569dbb9c6f48f58e4276b918
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-06-01 13:32:48 +0600

    Add files via upload

---
+++ Stack & Queue/Queue Implementation & STL/Q1-Implement_Stack_using_Queues.cpp
@@ -0,0 +1,33 @@
+class MyStack {
+public:
+    queue<int>q1;
+    MyStack() {
+        
+    }
+    void push(int x) {
+        q1.push(x);
+    }
+    
+    int pop() {
+        queue<int>q2;
+        int val;
+        while(!q1.empty()) {
+            val=q1.front();
+            q1.pop();
+            if(q1.empty()) {
+                break;
+            }
+            q2.push(val);
+        }
+        q1=q2;
+        return val;
+    }
+    
+    int top() {
+        return q1.back();
+    }
+    
+    bool empty() {
+        return q1.empty();
+    }
+};
