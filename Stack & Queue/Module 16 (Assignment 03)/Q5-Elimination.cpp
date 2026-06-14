c769bd4fd7acbf4ead7137b8e846d81936a58718
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-06-15 01:36:55 +0600

    Add files via upload

---
+++ Stack & Queue/Module 16 (Assignment 03)/Q5-Elimination.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+    int T;
+    cin>>T;
+    while(T--) {
+        string S;
+        cin>>S;
+        stack<char>st;
+        for(auto c :  S) {
+            if(st.empty()) {
+                st.push(c);
+            } else {
+                if(c=='1' && st.top()=='0') {
+                    st.pop();
+                } else {
+                    st.push(c);
+                }
+            }
+        }
+        if(st.empty()) {
+            cout<<"YES"<<endl;
+        } else {
+            cout<<"NO"<<endl;
+        }    
+    }
+    return 0;
+}
