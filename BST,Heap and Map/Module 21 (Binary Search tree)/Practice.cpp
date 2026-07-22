d4876487e4f3fa2dd6fc5df1cfe35b70095cf44a
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-07-23 00:12:27 +0600

    Add files via upload

---
+++ BST,Heap and Map/Module 21 (Binary Search tree)/Practice.cpp
@@ -0,0 +1,96 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node {
+    public :
+        int val;
+        Node* left;
+        Node* right;
+
+        Node(int val) {
+            this->val=val;
+            this->left=NULL;
+            this->right=NULL;
+        }
+};
+Node* input_tree() {
+    int val;
+    cin>>val;
+    Node* root;
+    if(val==-1) {
+        root=NULL;
+    } else {
+        root=new Node(val);
+    }
+
+    queue<Node*>q;
+    if(root!=NULL) {
+        q.push(root);
+    }
+    while(!q.empty()) {
+
+        // 1.Node ber kore ana
+        Node* f=q.front();
+        q.pop();
+
+        // 2.Oi node niye kaj kora
+        int l,r;
+        cin>>l>>r;
+        Node* myLeft;
+        Node* myRight;
+
+        if(l==-1) {
+            myLeft=NULL;
+        } else {
+            myLeft=new Node(l);
+        }
+
+        if(r==-1) {
+            myRight=NULL;
+        } else {
+            myRight=new Node(r);
+        }
+
+        f->left=myLeft;
+        f->right=myRight;
+
+        // 3.children push kora
+        if(f->left!=NULL) {
+            q.push(f->left);
+        }
+        if(f->right!=NULL) {
+            q.push(f->right);
+        }
+    }
+    return root;
+}
+void level_order(Node* root) {
+    if(root==NULL) {
+        cout<<"No tree "<<endl;
+        return;
+    }
+    queue<Node*>q;
+    q.push(root);
+
+    while(!q.empty()) {
+        // 1. queue theke node ber kore ana
+        Node* f=q.front();
+        q.pop();
+
+        // Node niye kaj kora
+        cout<<f->val<<" ";
+
+        // children node push kora 
+
+        if(f->left!=NULL) {
+            q.push(f->left);
+        }
+        if(f->right!=NULL) {
+            q.push(f->right);
+        }
+    }
+}
+int main () {
+    Node* root=input_tree();
+    level_order(root);
+    return 0;
+}
